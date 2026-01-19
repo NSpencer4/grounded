264ea8a91e4153cc4a29d4fc58be92814371c7f4
NSpencer4 <n.chase.spencer@gmail.com>
2026-01-19 00:09:37 -0500

    Add secrets manager utility to fetch secrets by ID
    Introduce a utility function using AWS Secrets Manager SDK for retrieving secrets based on their IDs. This will streamline accessing and managing secrets across services.

---
+++ packages/server/shared/secrets-manager/index.ts
@@ -0,0 +1,17 @@
+import { GetSecretValueCommand, SecretsManagerClient } from '@aws-sdk/client-secrets-manager'
+
+export const fetchSecrets = async (secretIds: string[]) => {
+  const client = new SecretsManagerClient()
+
+  const promises = await Promise.all(
+    secretIds.map(async (secretId) => {
+      const response = await client.send(new GetSecretValueCommand({ secretId }))
+      return { secretId, secretValue: response.SecretString }
+    }),
+  )
+
+  return promises.reduce((acc, { secretId, secretValue }) => {
+    acc[secretId] = secretValue
+    return acc
+  }, {})
+}
